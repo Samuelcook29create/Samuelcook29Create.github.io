685168681fdae2b696b686a1bbf2b3fad5cb4bc6
Sam cook <samuel.cook29@student.tamdistrict.org>
2025-11-18 17:52:55 +0000

    part 1 success

--- widget_project/script.js
+++ widget_project/script.js
@@ -1,4 +1,10 @@
-
-
-
-
+const Word1=document.getElementById("word1");
+const Word2=document.getElementById("word2");
+const Word3=document.getElementById("word3");
+const Word4=document.getElementById("word4");
+const wordOutput=document.getElementById("wordoutput");
+const generateBtn=document.getElementById("generatebtn")
+function generate(){
+    wordOutput.textContent= Word2.value+" " + Word4.value+" " +Word3.value+" "+Word1.value
+}
+generateBtn.addEventListener("click", generate)
